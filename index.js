df403f63d15d78c5b62698e7c622923dd4171862
anne <richardson.ae@gmail.com>
2017-08-14 15:36:13 -0500

    Require packages

---
+++ index.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+// require cli package
+var program = require('commander');
+
+// require api equest package
+var request = require('request');
+
+// require GitHub class
+var GitHub = require('./lib/github_api');
+
+// pull in GitHub User Agent for header
+var {USER_AGENT} = require('./secrets');
