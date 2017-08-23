af5f76cd6f3ad2a33a8786e0f2e30b903116ece4
Coelacanth7 <galford151@gmail.com>
2017-08-22 20:11:21 -0700

    almost done

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 //had some help from here, thanks learned a lot:
 //https://developer.atlassian.com/blog/2015/11/scripting-with-node/
 //comoman line interface tool
+//https://github.com/tj/commander.js/
 var program = require('commander');
 
 //request lib
@@ -14,48 +15,58 @@ const gitHuhModule = require('./api_wrapper');
 //set up new gitHuh
 const gitHuh = new gitHuhModule("coelacanth7")
 
-
 //cli params
 program
   .arguments('[cmd] [username]')
-//.option('-r, --repos <repos>', 'option to return a list of repos')
-//.option('-s, --stars <stars>', 'option to return starred repositories')
-//.option('-p, --profile <profile>', 'option to return users profile info')
   .action(function(cmd, username) {
+
     console.log('I am getting the %s info from %s', cmd, username )
 
-    switch (cmd){
+    //wrong params action
+    if (typeof cmd === 'undefined' || typeof username === 'undefined') {
+      console.log('Invalid command given');
+      console.log('To run githuh fire:');
+      console.log('githuh ( profile || repos || starred ) ( username )');
+      console.log('but without the parenthesis');
+      process.exit(1);
+    } else {
 
-      case "profile":
+        switch (cmd){
 
-        gitHuh.getUserProfileInfo(username, function(data) {
-          console.log('USER INFO:');
-          console.log("=============");
-          console.log(`USERNAME: ${data.login}`);
-          console.log(`NAME: ${data.name}`);
-          console.log(`EMAIL: ${data.email}`);
-          console.log(`FOLLOWERS: ${data.followers}`);
-          console.log(`FOLLOWING: ${data.following}`);
-          console.log(`NUMBER OF REPOS: ${data.public_repos}`);
-        });
+          case "profile":
 
-        break;
+            gitHuh.getUserProfileInfo(username, function(data) {
+              console.log('USER INFO:');
+              console.log("=============");
+              console.log(`USERNAME: ${data.login}`);
+              console.log(`NAME: ${data.name}`);
+              console.log(`EMAIL: ${data.email}`);
+              console.log(`FOLLOWERS: ${data.followers}`);
+              console.log(`FOLLOWING: ${data.following}`);
+              console.log(`NUMBER OF REPOS: ${data.public_repos}`);
+            });
 
-      case "repos":
+            break;
 
-        gitHuh.getRepos(username, function(data) {
-          console.log('LIST OF REPOS FOR ' + username);
-          console.log("=============");
-          data.forEach(function(repo){ console.log(repo.name) });
-        });
+          case "repos":
 
-        break;
+            gitHuh.getRepos(username, function(data) {
+              console.log('LIST OF REPOS FOR ' + username);
+              console.log("=============");
+              data.forEach(function(repo){ console.log(repo.name) });
+            });
 
-      case "starred":
+            break;
 
-        gitHuh.get
-    }
+          case "starred":
 
+            gitHuh.getStarred(username, function(data) {
+              console.log(`LIST OF STARRED REPOS FOR ${username}`);
+              console.log('=============');
+              data.forEach(function(starred){ console.log(starred.name) });
+            });
+        }//end switch
+      }//end if else
 
   })//end .action
   .parse(process.argv);
