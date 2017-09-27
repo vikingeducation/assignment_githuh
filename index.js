02a71cdfe092dc6b5a0200d9137f1850ef772bb6
Jared Gebel <jaredjgebel@gmail.com>
2017-09-27 10:56:58 -0400

    Add basic API request structure

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 var request = require("request");
+var _ = require("underscore");
 
-var pull = request.get();
+var api = "https://api.github.com/users/";
+var user = "jaredjgebel/";
+var section = "repos";
+
+var options = {
+  method: "GET",
+  url: api + user + section,
+  headers: {
+    accept: "application/vnd.github.v3+json",
+    "User-Agent": "b3a5623b072a615ce663366d750cc25e1c6de8c3"
+  }
+};
+
+request(options, function(error, response, body) {
+  if (error) throw new Error(error);
+
+  var obj = JSON.parse(body);
+  var userObject = {};
+  obj.forEach(function(obj) {
+    var repoDate = obj.created_at;
+    var repoName = obj.name;
+    var pairArray = [];
+    pairArray.push(repoDate);
+    pairArray.push(repoName);
+    userObject.push(pairArray);
+  });
+  console.log(dateArray);
+});
