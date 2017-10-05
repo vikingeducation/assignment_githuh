88554aeb73b6627e2b60b24653d1c7f75f392864
Steven Zarrella <stevenvz@users.noreply.github.com>
2017-10-05 10:52:12 -0400

    CLI working

--- git_api.js
+++ git_api.js
@@ -1,8 +1,18 @@
+#!/usr/bin/env node
+
 "use strict";
 
 const request = require('request');
 const baseUri = "https://api.github.com";
 const key = require('./config/setup');
+const cliArgs = process.argv;
+
+const cliEntry = {
+	cliCommand: cliArgs[2],
+	cliUserName: cliArgs[3]
+} //cliEntry
+
+const command = cliEntry.cliCommand;
 
 class GitHuh {
 	constructor(apiKey) {
@@ -10,7 +20,7 @@ class GitHuh {
 	}
 
 	repos(user, callback) {
-		this._sendRequest("repos", user, callback);
+		this._sendRequest(cliEntry.cliCommand, cliEntry.cliUserName, "?sort=pushed", callback);
 	}
 
 	stars(user, callback) {
@@ -21,23 +31,18 @@ class GitHuh {
 
 	}
 
-	_sendRequest(type, user, callback) {
-    const url = `${baseUri}/users/${user}/${type}`;
+	_sendRequest(type, user, parameters, callback) {
+    const url = `${baseUri}/users/${user}/${type}${parameters}`;
     const options = {
     	url: url,
     	headers: {
     		"User-Agent": "stevenvz",
-    		"Authorization": this.apiKey,
-    		"sort": "pushed"
+    		"Authorization": this.apiKey
     		}
     	};
-    console.log(options);
 
     request.get(options, function(error, response, body) {
-     
-      console.log(response.statusCode);
       if (!error & response.statusCode === 200) {
-        
         callback(JSON.parse(response.body));
         
       }
@@ -47,13 +52,43 @@ class GitHuh {
 } //GitHuh
 
 
-const callback = function(item) {
-	let repoNameArray = [];
-	item.forEach(function(element) {
-		repoNameArray.push(element.name);
-	});
-	console.log(repoNameArray);
-};
+const callbacks = {
+	reposCallback: function(item) {
+		let repoNameArray = [];
+		item.forEach(function(element) {
+			repoNameArray.push(element.name);
+		});
+		let recentFive = repoNameArray.splice(0, 5);
+		console.log("The 5 most recent repos for " + cliEntry.cliUserName.toUpperCase() + " are ===> ");
+		console.log(recentFive);
+	},
+
+} //callbacks
 
 const githuh = new GitHuh(key.key);
-githuh.repos("stevenvz", callback);
+
+if (command === "repos") {
+	githuh.repos(cliEntry.cliUserName, callbacks.reposCallback);
+} else if (command === "stars") {
+	githuh.stars(cliEntry.cliUserName, callback);
+} else if (command === "profile") {
+	githuh.profile(cliEntry.cliUserName, callback);
+} else {
+	console.log("'" + command + "'" + " is an invalid command");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
